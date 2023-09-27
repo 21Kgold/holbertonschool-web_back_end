26aba952f1893ab42e4dabcae18d3b2584bc863c
cau_rpa <claudiarpa@gmail.com>
2023-09-26 20:05:16 -0600

    Task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+3-to_str Module
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+    This is a type-annotated function that returns a string of a float number.
+
+    Parameters:
+    - n (float)
+
+    Returns:
+    - str: string representation of n
+    '''
+    return str(n)
