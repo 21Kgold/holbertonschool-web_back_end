afb41d8fcfb77f02cba0321310ae6d3823b1493a
cau_rpa <claudiarpa@gmail.com>
2023-09-27 15:03:12 -0600

    Task 0

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''
+0-basic_async_syntax Module
+'''
+import asyncio
+import random
+
+
+async def wait_random(max_delay: float = 10) -> float:
+    '''
+    This is a asynchronous coroutine with random delay between 0 and max_delay
+
+    Parameters:
+    - max_delay (float)
+
+    Returns:
+    - float: random delay between 0 and max_delay
+    '''
+    x: float = random.uniform(0, max_delay)
+    await asyncio.sleep(x)
+    return x
