ccd6dc106deee9da4af0579df2f0ed8700be2530
cau_rpa <claudiarpa@gmail.com>
2023-09-26 19:37:26 -0600

    Task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+1-concat Module
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    This is a type-annotated function that concat two parameters.
+
+    Parameters:
+    - str1 (string)
+    - str2 (string)
+
+    Returns:
+    - string: str1 + str2
+    '''
+    return str1 + str2
