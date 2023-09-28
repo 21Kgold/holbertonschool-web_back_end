c650b214f997ed943fc7cbcf8307bc241600cabc
cau_rpa <claudiarpa@gmail.com>
2023-09-27 22:32:33 -0600

    Task 2

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''
+2-measure_runtime Module
+'''
+from time import time
+import asyncio
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    t0 = time()
+    asyncio.run(wait_n(n, max_delay))
+    t1 = time()
+    dt = t1 - t0
+    return dt / n
