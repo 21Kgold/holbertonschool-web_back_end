9ce6e90485a11143f387de10cf0a6a38d8226a3c
cau_rpa <claudiarpa@gmail.com>
2023-09-28 00:06:49 -0600

    Task 4

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+'''
+1-concurrent_coroutines Module
+'''
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
+    '''
+    This is a async routine that calls a async function task_wait_random
+    n times concurrently (coroutines)
+
+    Parameters:
+    - n (int): times the async function task_wait_random will run concurrently
+    - max_delay (int): upper limit of the random delay used by the
+      task_wait_random function
+
+    Returns:
+    - List of floats: list of random delay for each routine
+    '''
+    tasks = [task_wait_random(max_delay) for i in range(n)]
+    delayList = []
+    for task in tasks:
+        delay_per_task = await task
+        delayList.append(delay_per_task)
+    return delayList
