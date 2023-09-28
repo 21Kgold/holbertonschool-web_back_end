c58e931503c3624b9b8028881c93859dd6e87729
cau_rpa <claudiarpa@gmail.com>
2023-09-27 18:43:29 -0600

    Task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -10,7 +10,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     '''
     This is a async routine that calls a async function wait_random
-    n times concurrently
+    n times concurrently (coroutines)
 
     Parameters:
     - n (int): times the async function wait_random will run concurrently
@@ -20,7 +20,12 @@ async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     Returns:
     - List of floats: list of random delay for each routine
     '''
-    # Create a list of n calls of the wait_random function
-    routineList = [wait_random(max_delay) for i in range(n)]
-    # The * operator unpack the list
-    return await asyncio.gather(*routineList)
+    # Create a list of n calls of wait_random function
+    n_calls = [wait_random(max_delay) for i in range(n)]
+
+    delayList = []
+    for call in asyncio.as_completed(n_calls):
+        # await, will pause a asynchronous operation until other is completed
+        delay_per_call = await call
+        delayList.append(delay_per_call)
+    return delayList
