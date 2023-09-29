e570c673bcc360b778291e43ea484eaccabfb132
cau_rpa <claudiarpa@gmail.com>
2023-09-29 17:54:24 -0600

    Task 10

---
+++ python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''
+100-safe_first_element Module
+'''
+from typing import Sequence, Union, Any
+
+
+def safe_first_element(lst: Sequence) -> Union[Any, None]:
+    '''
+    Returns first element of a secuence when present
+    '''
+    if lst:
+        return lst[0]
+    else:
+        return None
