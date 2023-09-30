e05bb0aeaf2ccbe67530b3dfb82505f226dbfa55
cau_rpa <claudiarpa@gmail.com>
2023-09-29 19:28:18 -0600

    Task 12

---
+++ python_variable_annotations/102-type_checking.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''
+102-type_checking.py Module
+'''
+from typing import List, Tuple
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    '''
+    Add factor times, each element of a given List to a new List
+    '''
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    print(zoomed_in)
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array, 2)
+
+zoom_3x = zoom_array(array, 3)
