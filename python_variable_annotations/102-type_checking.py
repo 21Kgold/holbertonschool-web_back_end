4c6ac42006638d53f6a022d831eec1b1fb0a6ef9
cau_rpa <claudiarpa@gmail.com>
2023-09-29 19:40:48 -0600

    Task 12

--- python_variable_annotations/102-type_checking.py
+++ python_variable_annotations/102-type_checking.py
@@ -5,19 +5,35 @@
 from typing import List, Tuple
 
 
-def zoom_array(lst: Tuple, factor: int = 2) -> List:
+def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
     '''
     Add factor times, each element of a given List to a new List
     '''
-    zoomed_in: List = [
+    zoomed_in: Tuple = [
         item for item in lst
         for i in range(factor)
     ]
     return zoomed_in
 
 
-array = (12, 72, 91)
+array = [12, 72, 91]
 
 zoom_2x = zoom_array(array, 2)
 
-zoom_3x = zoom_array(array, 3)
+zoom_3x = zoom_array(array, 3.0)
+
+'''
+102-type_checking.py:13: error: Incompatible types in assignment (expression
+has type "List[Any]", variable has type "Tuple[Any, ...]")  [assignment]
+
+102-type_checking.py:21: error: Argument 1 to "zoom_array" has incompatible
+type "List[int]"; expected "Tuple[Any, ...]"  [arg-type]
+
+102-type_checking.py:23: error: Argument 1 to "zoom_array" has incompatible
+type "List[int]"; expected "Tuple[Any, ...]"  [arg-type]
+
+102-type_checking.py:23: error: Argument 2 to "zoom_array" has incompatible
+type "float"; expected "int"  [arg-type]
+
+Found 4 errors in 1 file (checked 1 source file)
+'''
