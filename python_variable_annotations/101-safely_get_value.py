3b64f5c0948d9969cc1f15fadf36b8cfcd34d20a
cau_rpa <claudiarpa@gmail.com>
2023-09-29 18:47:10 -0600

    Task 11

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 '''
-9-element_length Module
+101-safely_get_value.py Module
 '''
 from typing import Any, Mapping, Union, TypeVar
 
 
 def safely_get_value(dct: Mapping, key: Any, default: Union[TypeVar('T'), None]
                      = None) -> Union[Any, TypeVar('T')]:
+    '''
+    Typed annotationed function that retrieves a dictionary key when present
+    '''
     if key in dct:
         return dct[key]
     else:
