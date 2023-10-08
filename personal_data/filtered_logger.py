654fcfad319bfe6674946524ed3c52684775ad58
cau_rpa <claudiarpa@gmail.com>
2023-10-08 09:09:14 -0600

    Task 1

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -2,7 +2,7 @@
 """
 filtered_logger module
 """
-from typing import List, Any
+from typing import List
 import re
 import logging
 
@@ -35,10 +35,10 @@ class RedactingFormatter(logging.Formatter):
     RedactingFormatter class
     """
 
-    REDACTION: str = "***"
-    FORMAT: str = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: \
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: \
                   %(message)s"
-    SEPARATOR: str = ";"
+    SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
         """
@@ -47,7 +47,7 @@ class RedactingFormatter(logging.Formatter):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
-    def format(self, record: logging.LogRecord[Any]) -> str:
+    def format(self, record: logging.LogRecord) -> str:
         """
         Filter values in incoming log record
         """
