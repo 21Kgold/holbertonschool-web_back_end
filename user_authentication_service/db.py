affbbd3741415cfd9ae916dc624ff254822ed6bd
cau_rpa <claudiarpa@gmail.com>
2023-10-15 11:16:20 -0600

    Update

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -2,13 +2,11 @@
 """DB module
 """
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
-
 from user import Base, User
+from typing import Union
 
 
 class DB:
@@ -18,7 +16,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db")
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -33,31 +31,33 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a new user
-        """
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
+        """ Add user in DB"""
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
         self._session.commit()
-        return new_user
+        return user
 
     def find_user_by(self, **kwargs) -> User:
-        """find the user"""
-        try:
-            record = self._session.query(User).filter_by(**kwargs).first()
-        except TypeError:
-            raise InvalidRequestError
-        if record is None:
+        """Find a user in the database
+        """
+        USER: Union[User, None] = self._session.query(User) \
+            .filter_by(**kwargs) \
+            .first()
+
+        if USER is None:
             raise NoResultFound
-        return record
+
+        return USER
 
     def update_user(self, user_id: int, **kwargs) -> None:
-        """ update user """
-        user_record = self.find_user_by(id=user_id)
+        """ Update user """
+        USER: User = self.find_user_by(id=user_id)
+
+        if 'id' in kwargs.keys():
+            raise ValueError("Cannot update 'id' of 'User'.")
 
         for key, value in kwargs.items():
-            if hasattr(user_record, key):
-                setattr(user_record, key, value)
+            if key in USER.__dict__.keys():
+                setattr(USER, key, value)
             else:
-                raise ValueError
-
-        self._session.commit()
+                raise ValueError(f"'User' object has no attribute '{key}'")
