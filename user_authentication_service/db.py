509bcc9ed973e8bd0ba0ec188bfa68a697f52b53
cau_rpa <claudiarpa@gmail.com>
2023-10-15 10:42:00 -0600

    Update

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -33,9 +35,29 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add a new user
         """
-        user = User()
-        user.email = email
-        user.hashed_password = hashed_password
-        self._session.add(user)
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
         self._session.commit()
-        return user
+        return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """find the user"""
+        try:
+            record = self._session.query(User).filter_by(**kwargs).first()
+        except TypeError:
+            raise InvalidRequestError
+        if record is None:
+            raise NoResultFound
+        return record
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """ update user """
+        user_record = self.find_user_by(id=user_id)
+
+        for key, value in kwargs.items():
+            if hasattr(user_record, key):
+                setattr(user_record, key, value)
+            else:
+                raise ValueError
+
+        self._session.commit()
