951b0e637a1d3436b08bb9d7e50c628c7b5ec754
cau_rpa <claudiarpa@gmail.com>
2023-10-15 09:21:21 -0600

    Task 1

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -33,7 +33,9 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add a new user
         """
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
+        user = User()
+        user.email = email
+        user.hashed_password = hashed_password
+        self._session.add(user)
         self._session.commit()
-        return new_user
+        return user
