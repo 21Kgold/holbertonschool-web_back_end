0ac05d58de663941f2a5ab5bc525313c6dfa349d
cau_rpa <claudiarpa@gmail.com>
2023-10-23 22:18:54 -0600

    Task 0

---
+++ i18n/0-app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Hello world basic flask app"""
+
+from flask import Flask, render_template
+app = Flask(__name__)  # instance of the Flask class is created
+
+
+@app.route('/')
+def Hello_World():
+    return render_template('0-index.html', message='Hello world')
+
+
+if __name__ == '__main__':  # Ensure the app is only run when executed directly
+    app.run(host="0.0.0.0", port="5000")  # http://localhost:5000
