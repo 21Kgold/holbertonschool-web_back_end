c2dbb41ffb212e578f9e68d8cd87f5cbf0198fde
cau_rpa <claudiarpa@gmail.com>
2023-10-24 11:29:09 -0600

    Task 2

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 """Babel default langage and timezone"""
+
 #  request represents the current HTTP request
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
+import inspect
+
+
+class Config:
+    """Class to configure available languages"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 
 app = Flask(__name__)
 babel = Babel(app)
-
-class Config():
-    """Class to configure available languages"""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
 app.config.from_object(Config)
 
 
@@ -22,14 +24,14 @@ def get_locale():
     """Function uses the request.accept_languages.best_match method to
     determine the best language match from provided options, based on the
     Accept-Language header sent by the browser."""
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(Config.LANGUAGES)
 
 
 @app.route('/')
 def Hello_World():
     """Basic Hello World app"""
     return render_template('2-index.html')
-    # to test: replace return line by: return get_locale()
+    # to test: replace return by: return get_locale()
 
 
 if __name__ == '__main__':  # Ensure the app is only run when executed directly
