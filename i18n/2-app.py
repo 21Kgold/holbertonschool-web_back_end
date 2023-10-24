364e3f37e8d069315f549d2195def9dc2ff674a3
cau_rpa <claudiarpa@gmail.com>
2023-10-24 11:17:08 -0600

    Task 2

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -6,9 +6,6 @@ from flask import Flask, render_template, request
 from flask_babel import Babel
 import inspect
 
-app = Flask(__name__)
-babel = Babel(app)
-
 
 class Config:
     """Class to configure available languages"""
@@ -17,22 +14,24 @@ class Config:
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+
 @babel.localeselector  # override the default behavior of Flask-Babel
 def get_locale():
     """Function uses the request.accept_languages.best_match method to
     determine the best language match from provided options, based on the
     Accept-Language header sent by the browser."""
-    return request.accept_languages.best_match(["en", "fr"])
-
-
-app.config.from_object(Config)
+    return request.accept_languages.best_match(Config.LANGUAGES)
 
 
 @app.route('/')
 def Hello_World():
     """Basic Hello World app"""
     return render_template('2-index.html')
-    # to test: replace retur by: return get_locale()
+    # to test: replace return line by: return get_locale()
 
 
 if __name__ == '__main__':  # Ensure the app is only run when executed directly
