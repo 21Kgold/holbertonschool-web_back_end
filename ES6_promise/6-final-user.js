39f527355cbdf82a9c8108f4ece6559d891718c0
cau_rpa <claudiarpa@gmail.com>
2023-11-18 14:15:26 -0600

    Task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,7 +1,7 @@
 import uploadPhoto from './5-photo-reject';
 import signUpUser from './4-user-promise';
 
-function handleProfileSignup(firstName, lastName, fileName) {
+function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
   const promise1 = uploadPhoto(fileName);
   const promise2 = signUpUser(firstName, lastName);
 
