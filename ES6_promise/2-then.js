80e1de2cbde3d997d4e2b4455b1f46559bf65583
cau_rpa <claudiarpa@gmail.com>
2023-11-17 20:00:14 -0600

    Task 2 use a promise

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,22 +1,10 @@
 function handleResponseFromAPI(promise) {
-  const myPromise = new Promise(((myResolve, myReject) => {
-    if (promise === true) {
-      myResolve({
-        status: 200,
-        body: 'Success',
-      });
-    } else {
-      myReject(new Error());
-    }
-  }));
-
-  /*eslint-disable*/
-    myPromise.then(
-      (value) => { value; },
-      (error) => { error; },
-    ).finally(() => console.log('Got a response from the API'));
-    return myPromise;
-  }
-  
-  export default handleResponseFromAPI;
-  
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
+}
+export default handleResponseFromAPI;
