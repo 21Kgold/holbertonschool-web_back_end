0ed6023afc486023bdbedd57b584ff5e205332c2
cau_rpa <claudiarpa@gmail.com>
2023-11-19 11:21:35 -0600

    Task 9

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,16 @@
+function guardrail(mathFunction) {
+  const queue = [];
+  let result;
+
+  try {
+    result = mathFunction();
+  } catch (error) {
+    result = error.toString();
+  }
+
+  queue.push(result);
+  queue.push('Guardrail was processed');
+  return queue;
+}
+
+export default guardrail;
