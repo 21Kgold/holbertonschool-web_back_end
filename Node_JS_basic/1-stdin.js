f0b68a47ce57bc0ce7c5040fd5fd443b80e941b0
cau_rpa <claudiarpa@gmail.com>
2023-11-27 09:17:27 -0600

    Task 1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('Welcome to Holberton School, what is your name? ', (name) => {
+  if (name.toLowerCase() === 'exit' || name.toLowerCase() === 'quit') {
+    console.log('This important software is now closing');
+    process.exit();
+  } else {
+    console.log(`Your name is: ${name}`);
+  }
+});
