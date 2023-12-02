22ef3539cd0102314a14b0cac010fcba76933e5e
cau_rpa <claudiarpa@gmail.com>
2023-12-01 21:58:03 -0600

    Task 3

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,49 @@
+const fs = require('fs').promises;
+
+async function countStudents(filePath) {
+  let fileContent;
+
+  try {
+    fileContent = await fs.readFile(filePath, 'utf8');
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+  const arrayOfLines = fileContent.split('\n');
+  const arrayOfHeaders = arrayOfLines[0].split(',');
+
+  const arraysOfRecords = arrayOfLines.slice(1)
+    .filter((row) => row.trim() !== '')
+    .map((row) => row.split(','));
+
+  const arrayOfStudentObjects = arraysOfRecords.map((array) => {
+    const obj = {};
+    arrayOfHeaders.forEach((key, index) => {
+      obj[key] = array[index];
+    });
+    return obj;
+  });
+
+  console.log(`Number of students: ${arrayOfStudentObjects.length}`);
+
+  const allFieldNames = arrayOfStudentObjects
+    .map((obj) => obj.field);
+
+  const fieldNames = new Set(allFieldNames);
+
+  // Object, where fieldName is the key and an array of firstnames is the value
+  const fieldsObject = {};
+
+  for (const fieldName of fieldNames) {
+    const filteredNames = arrayOfStudentObjects
+      .filter((obj) => obj.field === fieldName)
+      .map((obj) => obj.firstname);
+
+    fieldsObject[fieldName] = filteredNames;
+  }
+
+  Object.keys(fieldsObject).forEach((FIELD) => {
+    const LIST_OF_FIRSTNAMES = fieldsObject[FIELD].join(', ');
+    console.log(`Number of students in ${FIELD}: ${fieldsObject[FIELD].length}. List: ${LIST_OF_FIRSTNAMES}`);
+  });
+}
+module.exports = countStudents;
