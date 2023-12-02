9f675393d8eac41b9032304dd903642bc46d087c
cau_rpa <claudiarpa@gmail.com>
2023-12-02 17:55:26 -0600

    Task 5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,15 +1,25 @@
 const http = require('http');
-const { countStudents } = require('./3-read_file_async');
+const countStudents = require('./3-read_file_async');
+
+const args = process.argv;
+const databaseName = args[2];
+
+if (args.length < 3) {
+  console.error('Please provide the database name as an argument');
+  process.exit(1);
+}
 
 const app = http.createServer(async (req, res) => {
-  if (req.url === '/') {
+  const { url } = req;
+  if (url === '/') {
     res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
+  } else if (url === '/students') {
+    res.write('This is the list of our students');
     try {
-      const students = await countStudents('database.csv');
-      res.end(`This is the list of our students ${students}`);
+      const students = await countStudents(databaseName);
+      res.end(`${students}`);
     } catch (error) {
-      res.end('Error reading file');
+      res.end(error.message);
     }
   } else {
     res.end('404, Resource Not Found');
