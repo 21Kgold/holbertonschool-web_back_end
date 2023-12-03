69b7f96b1478da182508e04548916f8f29fcf5c9
cau_rpa <claudiarpa@gmail.com>
2023-12-02 19:24:40 -0600

    Task 5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -10,7 +10,7 @@ const app = http.createServer(async (req, res) => {
     res.write('This is the list of our students\n');
     try {
       const students = await countStudents(process.argv[2]);
-      res.end(`${students.join('\n')}`);
+      res.end(`${students}`);
     } catch (error) {
       res.end(error.message);
     }
