bf7ae9503f1ca9d57ce26419f7ff8c3c77f7c3b0
cau_rpa <claudiarpa@gmail.com>
2023-12-03 09:23:43 -0600

    Task 7

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  const firstLine = 'This is the list of our students\n';
+  try {
+    const students = await countStudents(process.argv[2]);
+    res.send(`${firstLine}${students.join('\n')}`);
+  } catch (error) {
+    res.send(`${firstLine}${error.message}`);
+  }
+});
+
+app.listen(1245, () => {
+  console.log('Server is running on port 1245');
+});
