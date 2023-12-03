ca553b6f3a999e8e9dd370d7da790bf963a9c177
cau_rpa <claudiarpa@gmail.com>
2023-12-02 18:22:16 -0600

    Task 5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,25 +1,22 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const args = process.argv;
-const databaseName = args[2];
-
 const app = http.createServer(async (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   if (req.url === '/') {
-    res.end('Hello Holberton School!');
+    res.write('Hello Holberton School!');
   } else if (req.url === '/students') {
     res.write('This is the list of our students\n');
     try {
-      const students = await countStudents(databaseName);
-      res.end(`${students.join('\n')}`);
+      const students = await countStudents(process.argv[2]);
+      res.write(`${students.join('\n')}`);
     } catch (error) {
       res.end(error.message);
     }
   } else {
     res.statusCode = 404;
-    res.end();
   }
+  res.end();
 });
 
 app.listen(1245);
