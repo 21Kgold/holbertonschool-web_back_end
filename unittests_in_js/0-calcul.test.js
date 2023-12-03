d064d8d6e83b020fd4d5bf74fe47bdeb340407cb
cau_rpa <claudiarpa@gmail.com>
2023-12-03 12:16:21 -0600

    Task 0

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -28,5 +28,17 @@ describe('calculateNumber', function () {
     assert.strictEqual(calculateNumber(3.7, 1.5), 6);
   });
 
+  it('Round to nearest the sum of one integer and one decimal', function () {
+    assert.strictEqual(calculateNumber(4, 0.9), 5);
+  });
+
+  it('Round to nearest even the sum of midpoint decimal', function () {
+    assert.strictEqual(calculateNumber(4, 1.5), 6);
+  });
+
+  it('Round to nearest even the sum of midpoint decimal', function () {
+    assert.strictEqual(calculateNumber(4, 0.5), 5);
+  });
+
   // Add more test cases as needed
 });
