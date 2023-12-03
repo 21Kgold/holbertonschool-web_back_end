f343b74f3316e3ae2489497b66359d4f4cc66cfe
cau_rpa <claudiarpa@gmail.com>
2023-12-02 19:10:16 -0600

    Task 5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -2,7 +2,8 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const app = http.createServer(async (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/') {
     res.write('Hello Holberton School!');
   } if (req.url === '/students') {
