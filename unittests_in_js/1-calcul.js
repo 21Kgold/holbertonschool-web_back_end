9cf3be36992178a28846a35a173a35a8354b4245
cau_rpa <claudiarpa@gmail.com>
2023-12-03 22:20:01 -0600

    Task 1

--- unittests_in_js/1-calcul.js
+++ unittests_in_js/1-calcul.js
@@ -1,12 +1,9 @@
 module.exports = function calculateNumber(type, a, b) {
-  const aNumber = Number(a);
-  const bNumber = Number(b);
-  const typeString = String(type);
+  const aNumber = Math.round(a);
+  const bNumber = Math.round(b);
 
-  if (Number.isNaN(aNumber) || Number.isNaN(bNumber)) { throw TypeError(`The provided parameter(s) ${a} ${b} must be numbers`); }
-  if (typeof typeString !== 'string') { throw TypeError(`The provided parameter ${type} must be string`); }
-  if (typeString === 'DIVIDE' && bNumber === 0) { return 'Error'; }
-  if (typeString === 'SUBTRACT') { return Math.round(aNumber) - Math.round(bNumber); }
-  if (typeString === 'DIVIDE') { return Math.round(aNumber) / Math.round(bNumber); }
-  return Math.round(aNumber) + Math.round(bNumber);
+  if (type === 'DIVIDE' && bNumber === 0) { return 'Error'; }
+  if (type === 'SUBTRACT') { return aNumber - bNumber; }
+  if (type === 'DIVIDE') { return aNumber / bNumber; }
+  return aNumber + bNumber;
 };
--- unittests_in_js/1-calcul.test.js
+++ unittests_in_js/1-calcul.test.js
@@ -1,61 +1,91 @@
 // 1-calcul.test.js
-const assert = require('assert');
+const assert = require('assert').strict;
 const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber', () => {
-  it('round to nearest the sum of two integers', () => {
-    assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
+  describe('first argument is SUM', () => {
+    it('rounds to nearest integer the second argument: a', () => {
+      assert.strictEqual(calculateNumber('SUM', 3.7, 1), 5);
+      assert.strictEqual(calculateNumber('SUM', 4, 0.9), 5);
+      assert.strictEqual(calculateNumber('SUM', 1.5, 4), 6);
+      assert.strictEqual(calculateNumber('SUM', 0.5, 4), 5);
+    });
+
+    it('rounds to nearest integer the third argument: b', () => {
+      assert.strictEqual(calculateNumber('SUM', 1, 3.7), 5);
+      assert.strictEqual(calculateNumber('SUM', 4, 0.9), 5);
+      assert.strictEqual(calculateNumber('SUM', 4, 1.5), 6);
+      assert.strictEqual(calculateNumber('SUM', 4, 0.5), 5);
+    });
+
+    it('rounds integers', () => {
+      assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
+      assert.strictEqual(calculateNumber('SUM', 1.0, 4), 5);
+      assert.strictEqual(calculateNumber('SUM', 3, 3.0), 6);
+    });
+
+    it('rounds second and third parameter', () => {
+      assert.strictEqual(calculateNumber('SUM', 1.2, 3.7), 5);
+      assert.strictEqual(calculateNumber('SUM', 1.5, 3.7), 6);
+      assert.strictEqual(calculateNumber('SUM', 3.7, 1.2), 5);
+    });
   });
 
-  it('round to nearest the sum of one integer and one decimal', () => {
-    assert.strictEqual(calculateNumber('SUM', 1, 3.7), 5);
+  describe('first argument is SUBTRACT', () => {
+    it('rounds to nearest integer the second argument: a', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 3.7, 1), 3);
+      assert.strictEqual(calculateNumber('SUBTRACT', 4, 0.9), 3);
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.5, 4), -2);
+      assert.strictEqual(calculateNumber('SUBTRACT', 0.5, 4), -3);
+    });
+
+    it('rounds to nearest integer the third argument: b', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1, 3.7), -3);
+      assert.strictEqual(calculateNumber('SUBTRACT', 4, 0.9), 3);
+      assert.strictEqual(calculateNumber('SUBTRACT', 4, 1.5), 2);
+      assert.strictEqual(calculateNumber('SUBTRACT', 4, 0.5), 3);
+    });
+
+    it('rounds integers', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1, 3), -2);
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.0, 4), -3);
+      assert.strictEqual(calculateNumber('SUBTRACT', 3, 3.0), 0);
+    });
+
+    it('rounds second and third parameter', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.2, 3.7), -3);
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.5, 3.7), -2);
+      assert.strictEqual(calculateNumber('SUBTRACT', 3.7, 1.2), 3);
+    });
   });
 
-  it('round to nearest the sum of one decimal and one integer', () => {
-    assert.strictEqual(calculateNumber('SUM', 3.7, 1), 5);
-  });
+  describe('first argument is DIVIDE', () => {
+    it('rounds to nearest integer the second argument: a', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 2.0, 2), 1);
+      assert.strictEqual(calculateNumber('DIVIDE', 2.7, 2), 1.5);
+      assert.strictEqual(calculateNumber('DIVIDE', 2.2, 2), 1);
+      assert.strictEqual(calculateNumber('DIVIDE', 2.5, 2), 1.5);
+    });
 
-  it('round to nearest the sum of two decimals case2', () => {
-    assert.strictEqual(calculateNumber('SUM', 1.2, 3.7), 5);
-  });
+    it('rounds to nearest integer the third argument: b', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 6, 2.0), 3);
+      assert.strictEqual(calculateNumber('DIVIDE', 6, 2.2), 3);
+      assert.strictEqual(calculateNumber('DIVIDE', 6, 2.7), 2);
+      assert.strictEqual(calculateNumber('DIVIDE', 6, 2.5), 2);
+      assert.strictEqual(calculateNumber('DIVIDE', 6, 0.5), 6);
+    });
 
-  it('round to nearest even the sum of midpoint decimal', () => {
-    assert.strictEqual(calculateNumber('SUM', 1.5, 3.7), 6);
-  });
+    it('rounds second and third parameter', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 12.2, 2.1), 6);
+      assert.strictEqual(calculateNumber('DIVIDE', 12.5, 3.6), 3.25);
+      assert.strictEqual(calculateNumber('DIVIDE', 12.0, 3.3), 4);
+    });
 
-  it('round to nearest the sum of two decimals case3', () => {
-    assert.strictEqual(calculateNumber('SUM', 3.7, 1.2), 5);
-  });
-
-  it('round to nearest the sum of one integer and one decimal', () => {
-    assert.strictEqual(calculateNumber('SUM', 4, 0.9), 5);
-  });
-
-  it('round to nearest even the sum of midpoint decimal', () => {
-    assert.strictEqual(calculateNumber('SUM', 4, 1.5), 6);
-  });
-
-  it('round to nearest the sum of midpoint decimal case2', () => {
-    assert.strictEqual(calculateNumber('SUM', 4, 0.5), 5);
-  });
-
-  it('round to nearest even the subtract of midpoint decimal', () => {
-    assert.strictEqual(calculateNumber('SUBTRACT', 4, 0.5), 3);
-  });
-
-  it('round to nearest and divide of midpoint decimal', () => {
-    assert.strictEqual(calculateNumber('DIVIDE', 4, 0), 'Error');
-  });
-
-  it('round to nearest even the divide of midpoint decimal', () => {
-    assert.strictEqual(calculateNumber('DIVIDE', 4, 0.5), 4);
-  });
-
-  it('round to nearest even the divide of midpoint decimal', () => {
-    assert.strictEqual(calculateNumber('DIVIDE', 1.2, 0.5), 1);
-  });
-
-  it('round to nearest even the divide of midpoint decimal', () => {
-    assert.strictEqual(calculateNumber('DIVIDE', 4, 0), 'Error');
+    it('returns Error if b or round b is equal to 0', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 6, 0), 'Error');
+      assert.strictEqual(calculateNumber('DIVIDE', 6, 0.0), 'Error');
+      assert.strictEqual(calculateNumber('DIVIDE', 6, 0.4), 'Error');
+      assert.strictEqual(calculateNumber('DIVIDE', 6, 0.1), 'Error');
+    });
   });
 });
