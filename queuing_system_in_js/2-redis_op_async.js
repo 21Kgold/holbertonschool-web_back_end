10f3360cd75531df8ebf8319838eae26ba25623b
cau_rpa <claudiarpa@gmail.com>
2023-12-06 20:35:45 -0600

    Task 3

---
+++ queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,36 @@
+import redis from 'redis';
+import util from 'util';
+
+const client = redis.createClient();
+const getAsync = util.promisify(client.get).bind(client);
+
+client.on("error", (errorMessage) => {
+  console.log(`Redis client not connected to the server: ${errorMessage}`);
+});
+
+client.on("ready", () => {
+  console.log("Redis client connected to the server");
+});
+
+function setNewSchool(myKey, myValue) {
+  client.set(myKey, myValue, (error, reply) => {
+    if (error) {
+      console.log(`Error: ${error}`);
+    } else {
+        console.log(`Reply: ${reply}`);
+    }
+  });
+}
+
+async function displaySchoolValue(myKey) {
+  try {
+    const value = await getAsync(myKey);
+    console.log(value);
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
